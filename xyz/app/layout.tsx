79d491cd134f89d8aee646c2be76c06793fdd7bb
akshat1602 <akshatmtech18@gmail.com>
2024-10-04 16:56:31 +0530

    added all the required files for spotify clone

---
+++ xyz/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import {Figtree} from 'next/font/google'
+
+const font = Figtree ({ subsets : ['latin'] })
+
+const geistSans = ({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = ({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "For Music Lovers!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{                          
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className= {font.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
