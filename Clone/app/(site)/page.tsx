6b3f18f37bd09107943d573a95d1da6829351f0d
Akshat Trivedi <131957541+akshat1602@users.noreply.github.com>
2024-10-15 02:34:08 +0530

    update

--- Clone/app/(site)/page.tsx
+++ Clone/app/(site)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
           <div className="grid grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
             <ListItem
-              image="/image/liked.png"
+              image="/images/liked.jpeg"
               name="Liked Songs"
               href="liked"
             />
