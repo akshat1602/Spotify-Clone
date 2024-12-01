54a5a1f074932bfc286fdd40611c1b5fd11e353d
Akshat Trivedi <131957541+akshat1602@users.noreply.github.com>
2024-12-01 21:02:04 +0530

    update

--- Clone/app/(site)/page.tsx
+++ Clone/app/(site)/page.tsx
@@ -1,7 +1,13 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
+import { DiVim } from "react-icons/di";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -9,7 +15,7 @@ export default function Home() {
           <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
           <div className="grid grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
             <ListItem
-              image="/images/liked.jpeg"
+              image="/images/li.jpeg"
               name="Liked Songs"
               href="liked"
             />
@@ -20,9 +26,7 @@ export default function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
         </div>
-        <div>
-          List Of Songs!
-        </div>
+        <PageContent songs={songs}/>
       </div>
     </div>
   );
