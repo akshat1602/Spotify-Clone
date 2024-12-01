4ac5aadd29c4125eba2ce155c3ee87eae793b486
Akshat Trivedi <131957541+akshat1602@users.noreply.github.com>
2024-12-02 00:36:48 +0530

    update

--- Clone/components/Header.tsx
+++ Clone/components/Header.tsx
@@ -73,23 +73,24 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
 
         <div className="flex justify-between items-center gap-x-4">
           
-          {user ? (
+          {user ? 
+          (
             <div
             className="flex gap-x-4 items-center">
-              <button
+              <Button
               onClick={handleLogout}
-              className="bg-white px-6 py-2"
+              className="bg-white px-6 py-2 "
               >
               Logout
-              </button>
-              <button
+              </Button>
+              <Button
               onClick={() => router.push('/account')}
               className="bg-white">
                 <FaUserAlt/>
-              </button>
+              </Button>
               
             </div>
-  ) : (
+         ) : (
           
           <>
           <div>
