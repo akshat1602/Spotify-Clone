3fa5a1baeac10d0822240dc00e662a79d38c5a89
Akshat Trivedi <131957541+akshat1602@users.noreply.github.com>
2024-12-14 01:40:14 +0530

    update

--- Clone/app/account/page.tsx
+++ Clone/app/account/page.tsx
@@ -1,9 +1,27 @@
+import Header from "@/components/Header";
+import AccountContext from "./components/AccountContent";
+
 const Account = () => {
     return (
-        <div>
-        Account
+        <div
+        className="
+        bg-neutral-900
+        rounded-lg
+        h-full
+        w-full
+        overflow-hidden
+        overflow-y-auto
+        ">
+            <Header className="from-bg-neutral-900">
+                <div className="mb-2 flex flex-col gap-y-6">
+                    <h1 className="text-white text-3xl font-semibold">
+                       Account Settings
+                    </h1>
+                </div>
+            </Header>
+            <AccountContext/>
         </div>
     );
 }
-
-export default
+ 
+export default Account;
