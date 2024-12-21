650ae01e95da3e58fd152a9788c4bf7899ff4b5e
Akshat Trivedi <131957541+akshat1602@users.noreply.github.com>
2024-12-21 22:45:51 +0530

    update

--- Clone/app/layout.tsx
+++ Clone/app/layout.tsx
@@ -7,7 +7,7 @@ import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
-import getSongsByUserId from "@/actions/getSongsByUserid";
+import getSongsByUserId from "@/actions/getSongsByUserId";
 import Player from "@/components/Player";
 import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices";
 
